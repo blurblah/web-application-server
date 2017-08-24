18a28cbc1229753c72de935ca179ca0c952321de
Sunchan Lee <blurblah@blurblah.net>
2017-08-24 10:54:48 +0900

    index.html을 포함한 GET 요청에 대한 응답 구현. Content-Type 맞춰줌

--- README.md
+++ README.md
@@ -15,7 +15,9 @@
 * 각 요구사항을 구현하는 것이 중요한 것이 아니라 구현 과정을 통해 학습한 내용을 인식하는 것이 배움에 중요하다. 
 
 ### 요구사항 1 - http://localhost:8080/index.html로 접속시 응답
-* 
+* Header에서 request 분리
+* BufferedReader => Reader <= InputStreamReader - InputStream
+* index.html 요청시 css 등의 resource 요청도 들어올 수 있는데 응답 header의 Content-Type을 맞춰주어야 함
 
 ### 요구사항 2 - get 방식으로 회원가입
 * 
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,14 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static java.nio.file.Files.readAllBytes;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,19 +25,48 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line = reader.readLine();
+            if(line == null) {
+                return;
+            }
+
+            String[] reqTokens = line.split(" ");
+            String mimeType = "text/html";
+            while(!line.isEmpty()) {
+                log.debug(line);
+                line = reader.readLine();
+                if(line.startsWith("Accept: ")) {
+                    mimeType = line.substring("Accept: ".length()).split(",|;")[0].trim();
+                }
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+
+            if(reqTokens != null && reqTokens.length > 1 && reqTokens[0].equals("GET")) {
+                String requestUri = "/index.html";
+                if(!reqTokens[1].equals("/")) {
+                    requestUri = reqTokens[1];
+                }
+
+                File f = new File("./webapp" + requestUri);
+                if(f.exists() && f.isFile()) {
+                    body = Files.readAllBytes(f.toPath());
+                }
+            }
+
+            response200Header(dos, mimeType, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, String mimeType, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + mimeType + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
